b66e5a583ecc20ddb275e3610c72b438cab7f84f
Mohamed Hassan <mh16455@essex.ac.uk>
2018-12-09 08:48:12 +0000

    Added KeyListener class and updates

---
+++ src/New_Assignment/GameKeyListener.java
@@ -0,0 +1,39 @@
+package New_Assignment;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GameKeyListener implements KeyListener {
+    public String directionOfSnake = "right";
+    public GameKeyListener(){
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //The keyListener changes the directionOfSnake variable to the direction based on the key pressed
+        int keyPressed = e.getKeyCode();
+        if (keyPressed == KeyEvent.VK_RIGHT && !directionOfSnake.equals("left")){
+            directionOfSnake = "right";
+        }
+        if (keyPressed == KeyEvent.VK_LEFT && !directionOfSnake.equals("right")){
+            directionOfSnake = "left";
+        }
+        if (keyPressed == KeyEvent.VK_UP && !directionOfSnake.equals("down")){
+            directionOfSnake = "up";
+        }
+        if (keyPressed == KeyEvent.VK_DOWN && !directionOfSnake.equals("up")){
+            directionOfSnake = "down";
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
--- src/New_Assignment/GamePanel.java
+++ src/New_Assignment/GamePanel.java
@@ -2,26 +2,22 @@ package New_Assignment;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.Timer;
 
 
 public class GamePanel extends JPanel implements Runnable{
-    //These positions are the start default
+    //posX and posY positions are the start default
     private int posX = 10;
     private int posY = 10;
 
     private static final int WIDTH = 800, HEIGHT = 800;//size of the JPanel, these need to be the same as the JFrame
 
     private ArrayList<Snake> snakeLength;
-    private Snake newSnakePart;
 
     private boolean running = false;
-    private int defaultSnakeSize = 5;
-    private Thread thread;
-    public String directionOfSnake = "right";
+    private int defaultSnakeSize = 10;
+    private GameKeyListener key = new GameKeyListener();
+
 
 
     GamePanel(){
@@ -47,12 +43,12 @@ public class GamePanel extends JPanel implements Runnable{
         //Set the running as true
         running = true;
         //Create a new thread and start it
-        thread = new Thread(this, "running Game");
+        Thread thread = new Thread(this, "running Game");
         thread.start(); //Start the thread
     }
 
     public void run(){
-        while (true) {
+        while (running) {
             refresh();
             repaint();
         }
@@ -60,10 +56,11 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void stopGame(){
         running = false; //stop the game
-        thread.stop(); //Stop the tread
+        //thread.stop(); //Stop the tread
     }
 
     private void refresh(){
+        Snake newSnakePart;
         if (snakeLength.size() == 0){
             newSnakePart = new Snake(posX,posY,10);
             snakeLength.add(newSnakePart);
@@ -75,10 +72,10 @@ public class GamePanel extends JPanel implements Runnable{
         } catch (InterruptedException e) {
             System.out.println("Could not sleep for 60");
         }
-        if (directionOfSnake.equals("right")) posX++;
-            if (directionOfSnake.equals("left")) posX--;
-            if (directionOfSnake.equals("down")) posY ++;
-            if (directionOfSnake.equals("up")) posY --;
+        if (key.directionOfSnake.equals("right")) posX++;
+            if (key.directionOfSnake.equals("left")) posX--;
+            if (key.directionOfSnake.equals("down")) posY ++;
+            if (key.directionOfSnake.equals("up")) posY --;
 
             newSnakePart = new Snake(posX,posY,10);
             snakeLength.add(newSnakePart);
@@ -108,39 +105,7 @@ public class GamePanel extends JPanel implements Runnable{
             snakeSquare.draw(g);
         }
 
-        
+
 
     }
-
-    //The keyListener changes the directionOfSnake variable to the direction based on the key pressed
-    KeyListener key = new KeyListener() {
-        @Override
-        public void keyTyped(KeyEvent e) {
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            int keyPressed = e.getKeyCode();
-            if (keyPressed == KeyEvent.VK_RIGHT && !directionOfSnake.equals("left")){
-                directionOfSnake = "right";
-            }
-            if (keyPressed == KeyEvent.VK_LEFT && !directionOfSnake.equals("right")){
-                directionOfSnake = "left";
-            }
-            if (keyPressed == KeyEvent.VK_UP && !directionOfSnake.equals("down")){
-                directionOfSnake = "up";
-            }
-            if (keyPressed == KeyEvent.VK_DOWN && !directionOfSnake.equals("up")){
-                directionOfSnake = "down";
-            }
-
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-
-        }
-    };
-
 }
